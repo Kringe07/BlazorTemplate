5c6d9791adeb3fb54b9b8cd3c4edb0da2f75b475
Kris Gersen <kgersen@4PS.nl>
2024-11-20 09:57:37 +0100

    c

--- DataAccess/Migrations/20241120085612_Audits.Designer.cs
+++ DataAccess/Migrations/20241120085612_Audits.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectName.DataAccess.DatabaseContext;
 
 #nullable disable
 
-namespace ProjectName.Migrations
+namespace ProjectName.DataAccess.Migrations
 {
     [DbContext(typeof(ProjectNameContext))]
-    partial class ProjectNameContextModelSnapshot : ModelSnapshot
+    [Migration("20241120085612_Audits")]
+    partial class Audits
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
